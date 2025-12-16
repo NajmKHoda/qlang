e332b2e787f0b9fd48965f7952e527292e3e7e1b
Najm Hoda <najmkhoda@gmail.com>
2025-12-16 14:08:49 -0800

    Automate compilation

---
+++ Makefile
@@ -0,0 +1,32 @@
+CC=clang
+DEBUG=true
+RUNTIME_OBJ=./out/runtime.o
+RUNTIME_SRC=./runtime/runtime.c
+COMPILER_MANIFEST=./compiler/Cargo.toml
+PROGRAM_SRC=./main.ql
+PROGRAM_OBJ=./out/main
+
+ifeq ($(DEBUG),true)
+	COMPILER_OBJ=./compiler/target/debug/db-lang
+else
+	COMPILER_OBJ=./compiler/target/release/db-lang
+endif
+
+$(PROGRAM_OBJ): $(PROGRAM_SRC) compiler
+	@$(COMPILER_OBJ) $(PROGRAM_SRC) $(PROGRAM_OBJ)
+
+compiler: $(COMPILER_OBJ) $(RUNTIME_OBJ)
+
+run: $(PROGRAM_OBJ)
+	@$(PROGRAM_OBJ)
+
+$(COMPILER_OBJ):
+	@cargo build --manifest-path=$(COMPILER_MANIFEST) $(if $(DEBUG),,--release)
+
+$(RUNTIME_OBJ): $(RUNTIME_SRC)
+	@mkdir -p out
+	@$(CC) -c $(RUNTIME_SRC) -o $(RUNTIME_OBJ)
+
+clean:
+	@rm -rf out/
+	@cargo clean --manifest-path=$(COMPILER_MANIFEST)
--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,6 +1,8 @@
 use std::io::Read;
-use std::io::Error as IOError;
+use std::io::{Error as IOError, ErrorKind};
 use std::fs::File;
+use std::env::args;
+use std::process::Command;
 use lalrpop_util::lalrpop_mod;
 
 mod tokens;
@@ -9,20 +11,33 @@ mod codegen;
 lalrpop_mod!(pub simple);
 
 fn main() -> Result<(), IOError> {
+    let args: Vec<String> = args().collect();
+    if args.len() < 3 {
+        eprintln!("Usage: {} <source-file> <object-file>", args[0]);
+        return Err(IOError::new(ErrorKind::InvalidInput, "Not enough arguments"));
+    }
+
+    let source_filepath = &args[1];
+    let obj_filepath = &args[2];
+
     let parser = simple::ProgramParser::new();
     let mut source = String::new();
-    let mut file = File::open("main.ql")?;
+    let mut file = File::open(source_filepath)?;
     file.read_to_string(&mut source)?;
 
     let program = parser.parse(&source).map_err(|e| {
-        eprintln!("Failed to parse main.ql: {e}");
-        IOError::new(std::io::ErrorKind::InvalidData, "Parsing failed")
+        eprintln!("Failed to parse {source_filepath}: {e}");
+        IOError::new(ErrorKind::InvalidData, "Parsing failed")
     })?;
 
     codegen::gen_code(&program).map_err(|e| {
-        eprintln!("Failed to build main.ql: {e}");
-        IOError::new(std::io::ErrorKind::InvalidData, "Building failed")
+        eprintln!("Failed to build {source_filepath}: {e}");
+        IOError::new(ErrorKind::InvalidData, "Building failed")
     })?;
+
+    Command::new("cc")
+        .args(&["out/main.o", "out/runtime.o", "-o", obj_filepath])
+        .status()?;
 
     Ok(())
 }
